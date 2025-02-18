bff313174259b0a7cdd696e171b7083a3caeadc2
NahomeYo <NahomeYo2002@gmail.com>
2025-02-18 09:36:03 -0800

    Schema Draft!

--- Database/AcademiaOS.sql
+++ Database/AcademiaOS.sql
@@ -1,5 +1,5 @@
 --
--- File generated with SQLiteStudio v3.4.16 on Tue Feb 18 08:32:21 2025
+-- File generated with SQLiteStudio v3.4.16 on Tue Feb 18 09:17:08 2025
 --
 -- Text encoding used: UTF-8
 --
@@ -8,14 +8,15 @@ BEGIN TRANSACTION;
 
 -- Table: REFLECTION
 CREATE TABLE REFLECTION (
-    reflectionId     INTEGER PRIMARY KEY AUTOINCREMENT,
+    reflectionId     INTEGER PRIMARY KEY,
     taskId           INTEGER REFERENCES TASK (taskID),
     moodCategory     TEXT,
-    entryJournalText TEXT,
+    entryJournalText TEXT    NOT NULL,
     moodRating       INTEGER CHECK (moodRating BETWEEN 1 AND 5),
     date             TEXT
 );
 
+INSERT INTO REFLECTION (reflectionId, taskId, moodCategory, entryJournalText, moodRating, date) VALUES (1, 1, NULL, 'Schema design needs more queries.', 4, NULL);
 
 -- Table: SCHEDULE
 CREATE TABLE SCHEDULE (
@@ -28,8 +29,8 @@ CREATE TABLE SCHEDULE (
     recurrencePattern TEXT    CHECK (recurrencePattern IN ('daily', 'weekly', 'monthly') ) 
 );
 
-INSERT INTO SCHEDULE (scheduleId, title, description, date, isRecurring, recurrencePattern) VALUES (1, 'CSC 3220', NULL, '2025-02-15', 0, NULL);
-INSERT INTO SCHEDULE (scheduleId, title, description, date, isRecurring, recurrencePattern) VALUES (2, 'CSC 3220', NULL, '2025-02-15', 0, NULL);
+INSERT INTO SCHEDULE (scheduleId, title, description, date, isRecurring, recurrencePattern) VALUES (3, 'CSC 3220', NULL, '2025-02-15', 1, 'weekly');
+INSERT INTO SCHEDULE (scheduleId, title, description, date, isRecurring, recurrencePattern) VALUES (4, 'UFDN 3100', NULL, '2025-02-15', 1, 'daily');
 
 -- Table: TASK
 CREATE TABLE TASK (
@@ -47,6 +48,7 @@ CREATE TABLE TASK (
 );
 
 INSERT INTO TASK (taskID, scheduleId, title, deadline, priority, duration, deadlineCountDown, status, startTime, endTime) VALUES (1, 1, 'Final DB Schema and Queries', '2025-02-19', 3, NULL, NULL, 'Completed', NULL, NULL);
+INSERT INTO TASK (taskID, scheduleId, title, deadline, priority, duration, deadlineCountDown, status, startTime, endTime) VALUES (2, 1, 'Draft DB Schema', '2025-02-17', 5, NULL, NULL, 'Completed', NULL, NULL);
 
 COMMIT TRANSACTION;
 PRAGMA foreign_keys = on;
