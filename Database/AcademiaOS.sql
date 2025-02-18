d637dbfa0d27a433f485729514914b761983cd19
NahomeYo <NahomeYo2002@gmail.com>
2025-02-18 08:33:20 -0800

    Schema draft!

--- Database/AcademiaOS.sql
+++ Database/AcademiaOS.sql
@@ -1,5 +1,5 @@
 --
--- File generated with SQLiteStudio v3.4.16 on Mon Feb 17 14:58:44 2025
+-- File generated with SQLiteStudio v3.4.16 on Tue Feb 18 08:32:21 2025
 --
 -- Text encoding used: UTF-8
 --
@@ -7,7 +7,7 @@ PRAGMA foreign_keys = off;
 BEGIN TRANSACTION;
 
 -- Table: REFLECTION
-CREATE TABLE IF NOT EXISTS REFLECTION (
+CREATE TABLE REFLECTION (
     reflectionId     INTEGER PRIMARY KEY AUTOINCREMENT,
     taskId           INTEGER REFERENCES TASK (taskID),
     moodCategory     TEXT,
@@ -16,19 +16,23 @@ CREATE TABLE IF NOT EXISTS REFLECTION (
     date             TEXT
 );
 
+
 -- Table: SCHEDULE
-CREATE TABLE IF NOT EXISTS SCHEDULE (
+CREATE TABLE SCHEDULE (
     scheduleId        INTEGER PRIMARY KEY AUTOINCREMENT,
     title             TEXT    NOT NULL,
     description       TEXT,
-    date              TEXT    NOT NULL,
+    date              TEXT,
     isRecurring       INTEGER DEFAULT (0) 
                               CHECK (isRecurring IN (0, 1) ),
     recurrencePattern TEXT    CHECK (recurrencePattern IN ('daily', 'weekly', 'monthly') ) 
 );
 
+INSERT INTO SCHEDULE (scheduleId, title, description, date, isRecurring, recurrencePattern) VALUES (1, 'CSC 3220', NULL, '2025-02-15', 0, NULL);
+INSERT INTO SCHEDULE (scheduleId, title, description, date, isRecurring, recurrencePattern) VALUES (2, 'CSC 3220', NULL, '2025-02-15', 0, NULL);
+
 -- Table: TASK
-CREATE TABLE IF NOT EXISTS TASK (
+CREATE TABLE TASK (
     taskID            INTEGER PRIMARY KEY AUTOINCREMENT,
     scheduleId        INTEGER REFERENCES SCHEDULE (scheduleId),
     title             TEXT    NOT NULL,
@@ -41,6 +45,8 @@ CREATE TABLE IF NOT EXISTS TASK (
     startTime         TEXT,
     endTime           TEXT
 );
+
+INSERT INTO TASK (taskID, scheduleId, title, deadline, priority, duration, deadlineCountDown, status, startTime, endTime) VALUES (1, 1, 'Final DB Schema and Queries', '2025-02-19', 3, NULL, NULL, 'Completed', NULL, NULL);
 
 COMMIT TRANSACTION;
 PRAGMA foreign_keys = on;
