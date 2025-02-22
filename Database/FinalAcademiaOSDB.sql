e4d0f44e0dbd9cc722ebabdf39d4df81f664f436
NahomeYo <NahomeYo2002@gmail.com>
2025-02-22 12:35:42 -0800

    DB final!

--- Database/FinalAcademiaOSDB.sql
+++ Database/FinalAcademiaOSDB.sql
@@ -1,45 +1,51 @@
 --
--- File generated with SQLiteStudio v3.4.16 on Fri Feb 21 23:57:02 2025
+-- File generated with SQLiteStudio v3.4.16 on Fri Feb 21 11:58:36 2025
 --
 -- Text encoding used: UTF-8
 --
 PRAGMA foreign_keys = off;
 BEGIN TRANSACTION;
 
--- Table: SCHEDULE
-CREATE TABLE SCHEDULE (
+-- Table: Schedule
+CREATE TABLE Schedule (
     scheduleId        INTEGER PRIMARY KEY AUTOINCREMENT,
     title             TEXT    NOT NULL,
     description       TEXT,
+    date              TEXT    NOT NULL
+                              CHECK (date GLOB '????-??-??'),
     isRecurring       INTEGER DEFAULT (0) 
-                              CHECK (isRecurring IN (0, 1) ) 
-                              NOT NULL,
+                              CHECK (isRecurring IN (0, 1) ),
     recurrencePattern TEXT    CHECK (recurrencePattern IN ('daily', 'weekly', 'monthly') ) 
 );
 
-INSERT INTO SCHEDULE (scheduleId, title, description, isRecurring, recurrencePattern) VALUES (1, 'CSC 3220', NULL, 0, NULL);
-INSERT INTO SCHEDULE (scheduleId, title, description, isRecurring, recurrencePattern) VALUES (2, 'CSC 3220', NULL, 0, NULL);
-INSERT INTO SCHEDULE (scheduleId, title, description, isRecurring, recurrencePattern) VALUES (3, 'CSC 3220', NULL, 1, 'weekly');
-INSERT INTO SCHEDULE (scheduleId, title, description, isRecurring, recurrencePattern) VALUES (4, 'UFDN 3100', NULL, 1, 'daily');
 
 -- Table: TASK
 CREATE TABLE TASK (
-    taskID            INTEGER  PRIMARY KEY AUTOINCREMENT,
-    scheduleId        INTEGER  REFERENCES SCHEDULE (scheduleId),
-    title             TEXT     NOT NULL,
-    deadline          DATETIME NOT NULL,
-    priority          INTEGER  CHECK (priority BETWEEN 1 AND 5),
-    deadlineCountDown INTEGER,
-    status            TEXT     DEFAULT Pending
-                               CHECK (status IN ('Pending', 'In Progress', 'Completed') ),
-    startTime         TEXT,
-    endTime           TEXT,
+    taskID            INTEGER PRIMARY KEY AUTOINCREMENT,
+    scheduleId        INTEGER NOT NULL
+                              REFERENCES Schedule (scheduleId),
+    title             TEXT    NOT NULL,
+    deadline          INTEGER NOT NULL
+                              CHECK (deadline > strftime('%s', 'now') ),
+    priority          INTEGER CHECK (priority BETWEEN 1 AND 5),
+    createdAt         INTEGER NOT NULL
+                              DEFAULT (strftime('%s', 'now') ),
+    startTime         INTEGER CHECK (startTime >= createdAt) 
+                              NOT NULL,
+    endTime           INTEGER CHECK (endTime > startTime) 
+                              NOT NULL,
+    duration          INTEGER AS ( (endTime - startTime) ) STORED,
+    deadlineCountdown INTEGER AS (deadline - strftime('%s', 'now') ) STORED,
+    status            TEXT    DEFAULT ('Pending') 
+                              CHECK (status IN ('Pending', 'In Progress', 'Completed') ),
+    moodCategory      TEXT    AS (CASE WHEN duration > 9000 THEN 'Exhausted' WHEN duration BETWEEN 3600 AND 9000 THEN 'Fit' WHEN duration < 3600 THEN 'Energetic' ELSE NULL END) STORED,
+    energyScale       INTEGER CHECK (energyScale BETWEEN 1 AND 5),
     entryJournalText  TEXT,
-    moodRating        INTEGER  CHECK (moodRating BETWEEN 1 AND 5) 
+    motivationMessage TEXT    AS (CASE WHEN energyScale >= 4 AND
+                                            duration < 3600 THEN [You're tired and spent an hour or less on the assignment? Get some more work done ;)] WHEN energyScale >= 4 AND
+                                                                                                                                                             duration >= 3600 THEN [You could use a break! Relax ;)] ELSE NULL END) STORED
 );
 
-INSERT INTO TASK (taskID, scheduleId, title, deadline, priority, deadlineCountDown, status, startTime, endTime, entryJournalText, moodRating) VALUES (1, 1, 'Final DB Schema and Queries', '2025-02-19', 3, NULL, 'Completed', NULL, NULL, NULL, NULL);
-INSERT INTO TASK (taskID, scheduleId, title, deadline, priority, deadlineCountDown, status, startTime, endTime, entryJournalText, moodRating) VALUES (2, 1, 'Draft DB Schema', '2025-02-17', 5, NULL, 'Completed', NULL, NULL, NULL, NULL);
 
 COMMIT TRANSACTION;
 PRAGMA foreign_keys = on;
